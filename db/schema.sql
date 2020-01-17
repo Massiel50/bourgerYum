319cbd366d89a18169d16c262d93292dd83be182
Massiel50 <massielescano@yahoo.com>
2020-01-17 17:17:17 -0500

    updated schema to new col name for boolean

--- db/schema.sql
+++ db/schema.sql
@@ -6,6 +6,6 @@ DROP TABLE IF EXISTS burgers;
  CREATE TABLE burgers (
      id INTEGER NOT NULL AUTO_INCREMENT,
      burger_name VARCHAR(100) NOT NULL,
-     devoured BOOLEAN DEFAULT FALSE,
+     eaten BOOLEAN DEFAULT FALSE,
      PRIMARY KEY (id)
  );
