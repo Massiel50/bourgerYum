8ac5fb0f01ab4a1505eb689ce22c238fe0894319
Massiel50 <massielescano@yahoo.com>
2020-01-17 17:13:57 -0500

    set up router file in controller to op routes

---
+++ public/burger.js
@@ -0,0 +1,56 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+    $(".change-eaten").on("click", function(event) {
+      var id = $(this).data("id");
+      var newEaten = $(this).data("neweaten");
+  
+      var newEatenState = {
+        sleepy: newSleep
+      };
+  
+      // Send the PUT request.
+      $.ajax("/api/burger/" + id, {
+        type: "PUT",
+        data: newEatenState
+      }).then(
+        function() {
+          console.log("changed eaten to", newEaten);
+          location.reload();
+        }
+      );
+    });
+  
+    $(".create-form").on("submit", function(event) {
+      event.preventDefault();
+  
+      var newBurger = {
+        name: $("#bg").val().trim(),
+        sleepy: $("[name=burger]:checked").val().trim()
+      };
+  
+      // Send the POST request
+      $.ajax("/api/burger", {
+        type: "POST",
+        data: newCat
+      }).then(
+        function() {
+          console.log("created new burger");
+          location.reload();
+        }
+      );
+    });
+  
+    $(".delete-burger").on("click", function(event) {
+      var id = $(this).data("id");
+  
+      // Send the DELETE request.
+      $.ajax("/api/burger/" + id, {
+        type: "DELETE"
+      }).then(
+        function() {
+          console.log("deleted burger", id);
+          location.reload();
+        }
+      );
+    });
+  });
